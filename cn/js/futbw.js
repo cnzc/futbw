b7c98e3fe9c2164a0d175796a029eef7feae508d
CNZC <449549101@qq.com>
2022-11-07 09:27:54 +0800

    Update futbw.js

--- cn/js/futbw.js
+++ cn/js/futbw.js
@@ -384,10 +384,10 @@ function round2() {
 
 	document.getElementById("cardlake").innerHTML="欢迎来到 https://FUTBW.com\n";
 	mp1 = 4, hp1 = 5, ap1 = 3, thp1 = 0, cardtop1 = 0;
-	mp2 = 4, hp2 = 8, ap2 = 3, thp2 = 0, cardtop2 = 0;
+	mp2 = 6, hp2 = 10, ap2 = 3, thp2 = 0, cardtop2 = 0;
 	//
 	givecard(1, 4);
-	givecard(2, 4);
+	givecard(2, 6);
 	showhp();showcards();
 function zok() {
 	if (hp1 > 0 && hp2 > 0) {
